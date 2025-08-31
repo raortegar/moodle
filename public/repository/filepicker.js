5c0c799328a16e243054451da99ba36ce614b7c5
raortegar <raquel.ortega@moodle.com>
2025-08-31 21:44:29 +0200

    MDL-80447 core_repository: Add upload button to filepicker repository

--- public/repository/filepicker.js
+++ public/repository/filepicker.js
@@ -1632,6 +1632,8 @@ M.core_filepicker.init = function(Y, options) {
             this.active_repo.message = (data.message || '');
             this.active_repo.help = data.help?data.help:null;
             this.active_repo.manage = data.manage?data.manage:null;
+            this.active_repo.uploadfile = data.uploadfile ? data.uploadfile : null;
+            this.active_repo.uploadurl = data.uploadurl ? data.uploadurl : null;
             // Warning message related to the file reference option, if applicable to the given repository.
             this.active_repo.filereferencewarning = data.filereferencewarning ? data.filereferencewarning : null;
             this.print_header();
@@ -2003,6 +2005,163 @@ M.core_filepicker.init = function(Y, options) {
                     managelnk.simulate('click')
                 });
 
+            // New Upload functionality.
+            toolbar.one('.fp-tb-uploadfile').one('a,button').on('click', function (e) {
+                e.preventDefault();
+                var repoId = this.active_repo.id;
+                var contextId = this.options.context.id;
+                var bodyHtml = '<div class="dropzone-container"></div>';
+                var draftidRef = { value: null }; // store draftid between uploads
+                var uploadurl = this.active_repo.uploadurl;
+
+                require(['core/modal_factory', 'core/modal_events', 'core/dropzone', 'core/notification', 'core/str'],
+                    function (ModalFactory, ModalEvents, DropZone, Notification, Str) {
+                    // Create a Modal box with a dropzone where files can be uploaded.
+                    function openUploadModal() {
+                        ModalFactory.create({
+                            type: ModalFactory.types.SAVE_CANCEL,
+                            title: Str.get_string('upload'),
+                            body: bodyHtml,
+                            large: true
+                        }).then(function (modal) {
+                            modal.getRoot().on(ModalEvents.shown, function () {
+                                initDropzone(modal);
+                            });
+                            modal.getRoot().on(ModalEvents.hidden, function () {
+                                modal.destroy();
+                            });
+                            modal.getRoot().on(ModalEvents.save, function (ev) {
+                                commitFiles(ev, modal, toolbar);
+                            });
+                            modal.getRoot().on(ModalEvents.cancel, function () {
+                                modal.hide();
+                            });
+                            modal.show();
+                        });
+                    }
+                    openUploadModal();
+
+                    // Create dropzone inside the modal.
+                    function initDropzone(modal) {
+                        const $body = modal.getBody();
+                        $body.html(bodyHtml);
+
+                        const dropzoneContainer = $body.find('.dropzone-container').get(0);
+                        const dz = new DropZone(dropzoneContainer, '*', function (files) {
+                            handleDroppedFiles(files, modal);
+                        });
+
+                        // Asynchronously load and set the label.
+                        Str.get_string('dropfiles', 'repository').then(function(label) {
+                            dz.setLabel(label);
+                        });
+
+                        dz.init();
+                    }
+
+                    // Hande file upload to save it as draft.
+                    function handleDroppedFiles(files, modal) {
+                        Array.from(files).forEach(file => {
+                            var formData = new FormData();
+                            formData.append('repo_upload_file', file);
+                            formData.append('repo_id', repoId);
+                            formData.append('contextid', contextId);
+                            formData.append('sesskey', M.cfg.sesskey);
+                            formData.append('action', 'upload');
+                            if (draftidRef.value) {
+                                formData.append('itemid', draftidRef.value);
+                            }
+
+                            var xhr = new XMLHttpRequest();
+                            xhr.open('POST', M.cfg.wwwroot + uploadurl, true);
+
+                            xhr.onload = function () {
+                                let response = JSON.parse(xhr.responseText);
+                                if (response.error) {
+                                    Notification.alert(
+                                        Str.get_string('uploaderror', 'repository'),
+                                        response.error,
+                                        Str.get_string('close', 'repository')
+                                    );
+                                } else {
+                                    if (response.draftid) {
+                                        draftidRef.value = response.draftid;
+                                    }
+                                    renderUploadedFile(modal, response);
+                                }
+                            };
+
+                            xhr.send(formData);
+                        });
+                    }
+
+                    // Render the uploaded file in a given modal.
+                    function renderUploadedFile(modal, response) {
+                        const $body = modal.getBody();
+                        let $fileList = $body.find('.uploaded-files ul');
+
+                        if ($fileList.length === 0) {
+                             // Load the string asynchronously
+                            Str.get_string('attachedfiles', 'repository').then(function(label) {
+                                $body.append(
+                                    '<br/><div class="uploaded-files">' +
+                                        '<h5>' + label + '</h5>' +
+                                        '<ul></ul>' +
+                                    '</div>'
+                                );
+
+                                $fileList = $body.find('.uploaded-files ul');
+                                $fileList.append(`<li>${response.file}</li>`);
+                            });
+                        }
+
+                        $fileList.append(`<li>${response.file}</li>`);
+                    }
+
+                    //Commit uploaded draft files when user clicks save.
+                    function commitFiles(ev, modal, toolbar) {
+                        ev.preventDefault();
+
+                        if (!draftidRef.value) {
+                            Notification.alert(
+                                Str.get_string('uploaderror', 'repository'),
+                                Str.get_string('nofilesattached', 'repository'),
+                                Str.get_string('close', 'repository')
+                            );
+                            return;
+                        }
+
+                        var formData = new FormData();
+                        formData.append('action', 'commit');
+                        formData.append('repo_id', repoId);
+                        formData.append('contextid', contextId);
+                        formData.append('sesskey', M.cfg.sesskey);
+                        formData.append('itemid', draftidRef.value);
+
+                        var xhr = new XMLHttpRequest();
+                        xhr.open('POST', M.cfg.wwwroot + uploadurl, true);
+
+                        xhr.onload = function () {
+                            let response = JSON.parse(xhr.responseText);
+                            if (response.error) {
+                                Notification.alert(
+                                    Str.get_string('uploaderror', 'repository'),
+                                    response.error,
+                                    Str.get_string('close', 'repository')
+                                );
+                            } else {
+                                modal.hide();
+                                var refreshButton = toolbar.one('.fp-tb-refresh').one('a,button');
+                                if (refreshButton) {
+                                    refreshButton.simulate('click');
+                                }
+                            }
+                        };
+                        xhr.send(formData);
+                    }
+                });
+            }, this);
+
             // same with .fp-tb-help
             var helplnk = Y.Node.create('<a/>').
                 setAttrs({id:'fp-tb-help-'+client_id+'-link', target:'_blank'}).
@@ -2078,6 +2237,9 @@ M.core_filepicker.init = function(Y, options) {
             // manage url
             enable_tb_control(toolbar.one('.fp-tb-manage'), r.manage);
             Y.one('#fp-tb-manage-'+client_id+'-link').set('href', r.manage);
+
+            // Upload file.
+            enable_tb_control(toolbar.one('.fp-tb-uploadfile'), r.uploadfile);
 
             // help url
             enable_tb_control(toolbar.one('.fp-tb-help'), r.help);
